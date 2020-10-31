fae95aa4181fa2e98b8c307fc4ba50fb5c830257
pratheekevive <pratheek.i@goevive.com>
2020-11-01 02:08:27 +0530

    Add package a

---
+++ a/a.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/pratheekevive/submod/b"
+)
+
+const Name = b.Name
+
+func main() {
+	fmt.Print(Name)
+}
---
+++ a/go.mod
@@ -0,0 +1,5 @@
+module github.com/pratheekevive/submod/a
+
+go 1.15
+
+require github.com/pratheekevive/submod/b v0.1.0
